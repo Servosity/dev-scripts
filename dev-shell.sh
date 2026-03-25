8b6ff229db6a931666521e2960b2819b6f03fb1a
Daniel Jones <tortxof@gmail.com>
2026-03-25 11:02:43 -0400

    Add feedback and window title to dev-shell.sh

--- dev-shell.sh
+++ dev-shell.sh
@@ -20,9 +20,22 @@ if [ -z "$SVT_SECRET_PROJECT" ]; then
     exit 1
 fi
 
-aws secretsmanager get-secret-value \
+secret_json=$(aws secretsmanager get-secret-value \
     --secret-id "$SVT_SECRET_ENV_NAME/$SVT_SECRET_PROJECT/env" \
     --query SecretString \
     --output text \
-    --profile svt-dev \
-    | json-env-helper -- bash
+    --profile svt-dev)
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to retrieve secret from AWS Secrets Manager." >&2
+    exit 1
+fi
+
+echo -ne "\033]0;dev-shell: $SVT_SECRET_PROJECT\007"  # Set window title
+echo "*** Dev shell ready ***"
+echo "Project: $SVT_SECRET_PROJECT"
+echo "Secret: $SVT_SECRET_ENV_NAME"
+echo
+echo "$secret_json" | json-env-helper -- bash
+echo -ne "\033]0;\007"  # Clear window title, letting the terminal revert to default
+echo "*** Dev shell exited ***"
